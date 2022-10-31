4ae51c40df3019ff528027d8d2932b14fefc34ec
ggorr <ggorr.kp@gmail.com>
2022-10-31 11:51:36 +0900

    Update type3.ts

--- ts/type3.ts
+++ ts/type3.ts
@@ -20,7 +20,7 @@ function displayIII1() {
     dgs[0].setIter(50);
 
     dgs[0].F.setValue([1, 0, 0, 1.3, 0, 0]);
-    dgs[0].F.setUsage('XXXFXX');
+    dgs[0].F.setUsage('XXXAXX');
     dgs[0].F.avoid1(3);
 
     dgs[0].G.setValue([1, 0, 0, 1.2, 1, 0]);
@@ -30,18 +30,18 @@ function displayIII1() {
     dgs[1].setIter(50);
 
     dgs[1].F.setValue([1, 0, 0, 1.2, 0, 0]);
-    dgs[1].F.setUsage('XXXFXX');
+    dgs[1].F.setUsage('XXXAXX');
     dgs[1].F.avoid1(3);
 
     dgs[1].G.setValue([2, 0, 0, 3, 0, 0]);
-    dgs[1].G.setUsage('FXXFXX');
+    dgs[1].G.setUsage('AXXFXX');
     dgs[1].G.avoid1(0);
 
     dgs[2].setVisible(true);
     dgs[2].setIter(100);
 
     dgs[2].F.setValue([1, 0, 0, 1.1, 0, 0]);
-    dgs[2].F.setUsage('XXXFXX');
+    dgs[2].F.setUsage('XXXAXX');
     dgs[2].F.avoid1(3);
 
     dgs[2].G.setValue([1.2, 0, 0, 1.2, 2, 0]);
@@ -54,7 +54,7 @@ function displayIII2() {
     dgs[0].setIter(30);
 
     dgs[0].F.setValue([1, 0, 0, 2, 1, 0]);
-    dgs[0].F.setUsage('XXXFXX');
+    dgs[0].F.setUsage('XXXAXX');
     dgs[0].F.avoid1(3);
 
     dgs[0].G.setValue([1, 0, 0, 1.2, 1.4, 0]);
@@ -66,7 +66,7 @@ function displayIII3() {
     dgs[0].setIter(20);
 
     dgs[0].F.setValue([2, 0, 0, 1.5, 0, 0]);
-    dgs[0].F.setUsage('FXXFXX');
+    dgs[0].F.setUsage('AXXBXX');
     dgs[0].F.avoid1Equal(0, 3);
 
     dgs[0].G.setValue([1.2, 0, 0, 1.6, 0, 0]);
