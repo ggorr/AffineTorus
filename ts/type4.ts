f4be354d2105f709aad655cb77625aa6e9c65f0e
ggorr <ggorr.kp@gmail.com>
2022-10-31 11:51:30 +0900

    Update type4.ts

--- ts/type4.ts
+++ ts/type4.ts
@@ -17,12 +17,12 @@ function displayIV() {
     dgs[0].setInit(1, 0);
 
     dgs[0].F.setValue(['cos(pi/6)', '-sin(pi/6)', 'sin(pi/6)', 'cos(pi/6)', '0', '0']);
-    dgs[0].F.setUsage('RRRRXX');
+    dgs[0].F.setUsage('RSSRXX');
     dgs[0].F.addRelation(0, 3);
     dgs[0].F.addRelation(1, 2, true);
 
     dgs[0].G.setValue([2, 0, 0, 2, 0, 0]);
-    dgs[0].G.setUsage('RRRRXX');
+    dgs[0].G.setUsage('RSSRXX');
     dgs[0].G.addRelation(0, 3);
     dgs[0].G.addRelation(1, 2, true);
 
@@ -31,12 +31,12 @@ function displayIV() {
     dgs[1].setInit(1, 0);
 
     dgs[1].F.setValue(['2cos(pi/6)', '-2sin(pi/6)', '2sin(pi/6)', '2cos(pi/6)', '0', '0']);
-    dgs[1].F.setUsage('RRRRXX');
+    dgs[1].F.setUsage('RSSRXX');
     dgs[1].F.addRelation(0, 3);
     dgs[1].F.addRelation(1, 2, true);
 
     dgs[1].G.setValue(['2cos(pi/6)', '2sin(pi/6)', '-2sin(pi/6)', '2cos(pi/6)', '0', '0']);
-    dgs[1].G.setUsage('RRRRXX');
+    dgs[1].G.setUsage('RSSRXX');
     dgs[1].G.addRelation(0, 3);
     dgs[1].G.addRelation(1, 2, true);
 
@@ -44,12 +44,12 @@ function displayIV() {
     dgs[2].setIter(100);
 
     dgs[2].F.setValue([2, -1, 1, 2, 0, 0]);
-    dgs[2].F.setUsage('RRRRXX');
+    dgs[2].F.setUsage('RSSRXX');
     dgs[2].F.addRelation(0, 3);
     dgs[2].F.addRelation(1, 2, true);
 
     dgs[2].G.setValue([2, 1, -1, 2, 0, 0]);
-    dgs[2].G.setUsage('RRRRXX');
+    dgs[2].G.setUsage('RSSRXX');
     dgs[2].G.addRelation(0, 3);
     dgs[2].G.addRelation(1, 2, true);
 
